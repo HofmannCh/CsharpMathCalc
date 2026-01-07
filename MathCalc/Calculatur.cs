b4ea0857345058fb71a835e45d0c165dcb513167
HofmannCH <lukas-hofmann@gmx.ch>
2026-01-07 11:01:57 +0100

    Added functions

--- MathCalc/Calculatur.cs
+++ MathCalc/Calculatur.cs
@@ -17,7 +17,8 @@ namespace MathCalc
             new (1, "-", (a,b) => a - b ),
             new (2, "*", (a,b) => a * b ),
             new (2, "/", (a,b) => a / b ),
-            new (2, "**", (a,b) => Math.Pow(a, b) ),
+            new (2, "**", Math.Pow),
+            new (2, "^", Math.Pow),
         }).OrderByDescending(x => x.Expression.Length)];
 
         [DebuggerDisplay("{DebuggerDisplay,nq}")]
@@ -31,13 +32,53 @@ namespace MathCalc
             public override string ToString() => DebuggerDisplay;
         }
 
+        private static readonly Random Random = new Random();
+
+        private static readonly Function[] Functions = [.. (new Function[]
+        {
+            new ("sqrt", 1, attrs => Math.Sqrt(attrs[0])),
+            new ("pow", 2, attrs => Math.Pow(attrs[0], attrs[1])),
+            new ("pi", 0, attrs => Math.PI),
+            new ("log", 1, attrs => Math.Log(attrs[0])),
+            new ("log", 2, attrs => Math.Log(attrs[0], attrs[1])),
+            new ("log2", 1, attrs => Math.Log2(attrs[0])),
+            new ("log10", 1, attrs => Math.Log10(attrs[0])),
+            new ("min", 2, attrs => Math.Min(attrs[0], attrs[1])),
+            new ("max", 2, attrs => Math.Max(attrs[0], attrs[1])),
+            new ("floor", 1, attrs => Math.Floor(attrs[0])),
+            new ("roundd", 1, attrs => Math.Floor(attrs[0])),
+            new ("ceil", 1, attrs => Math.Ceiling(attrs[0])),
+            new ("roundu", 1, attrs => Math.Ceiling(attrs[0])),
+            new ("round", 1, attrs => Math.Round(attrs[0])),
+            new ("abs", 1, attrs => Math.Abs(attrs[0])),
+            new ("sin", 1, attrs => Math.Sin(attrs[0])),
+            new ("cos", 1, attrs => Math.Cos(attrs[0])),
+            new ("tan", 1, attrs => Math.Tan(attrs[0])),
+            new ("tan", 1, attrs => Math.Tan(attrs[0])),
+            new ("rnd", 0, attrs => Random.NextDouble()),
+            new ("rndi", 0, attrs => Random.Next()),
+            new ("rndi", 1, attrs => Random.Next((int)attrs[0])),
+            new ("rndi", 2, attrs => Random.Next((int)attrs[0], (int)attrs[1])),
+        }).OrderByDescending(x => x.Name.Length)];
+
+        [DebuggerDisplay("{DebuggerDisplay,nq}")]
+        public class Function(string name, int countAttributes, Func<List<double>, double> action)
+        {
+            public string Name { get; set; } = name;
+            public int CountAttributes { get; set; } = countAttributes;
+            public Func<List<double>, double> Action { get; set; } = action;
+
+            public string DebuggerDisplay => $"FN {name}({CountAttributes}x)";
+            public override string ToString() => DebuggerDisplay;
+        }
+
         // 1 + 1 * 2
         // (1 + 1) * 2
 
         public double EvaluateExpression(string expression)
         {
             int pos = 0;
-            return EvaluateExpression(ref pos, expression);
+            return EvaluateExpression(ref pos, expression, EvalMod.Root);
         }
 
         // 4 + 2 * (3 + 1) / 4 = 3
@@ -45,20 +86,39 @@ namespace MathCalc
         // 1: 4+, 2*(3+1)/4
         // 2: 2*, (3+1)/, 4
         // 3: 3+, 1
-        public double EvaluateExpression(ref int pos, string expression)
-        {
 
-            string buildingNumber = string.Empty;
+        public enum ReadingMod
+        {
+            Value,
+            Brackets,
+            Operator,
+            FunctionName,
+            FunctionArgs,
+            FunctionArgsComma,
+        }
+
+        public enum EvalMod
+        {
+            Root,
+            Brackets,
+            FunctionArgs
+        }
+
+        private double EvaluateExpression(ref int pos, string expression, EvalMod evalMod)
+        {
+            string buildingValue = string.Empty;
             double? currentValue = null;
+            var readingMod = ReadingMod.Value;
 
             void MakeBuildNumber()
             {
-                currentValue = Convert.ToDouble("0" + buildingNumber.Replace(',', '.').TrimEnd('.'));
-                buildingNumber = string.Empty;
+                currentValue = Convert.ToDouble("0" + buildingValue.Replace(',', '.').TrimEnd('.'));
+                readingMod = ReadingMod.Operator;
+                buildingValue = string.Empty;
             }
 
-            var subExpressions = new List<(double Value, Operator? Operator)>(); // Value, Operation
-
+            var subExpressions = new List<(double Value, Operator? Operator)>(4); // Value, Operation
+            var argValues = new List<double>(4);
             var startPos = pos;
 
             double CalcCurrentVal(int pos)
@@ -85,9 +145,9 @@ namespace MathCalc
                     {
                         var currentItem = enumerator.Current;
 
-                        if (prevItem.Operator.Prio < prio)
+                        if (prevItem.Operator!.Prio < prio)
                         {
-                            yield return prevItem;
+                            yield return prevItem!;
                             prevItem = currentItem;
                         }
                         else
@@ -96,7 +156,7 @@ namespace MathCalc
                         }
                     }
 
-                    yield return prevItem;
+                    yield return prevItem!;
                 }
 
                 IEnumerable<(double Value, Operator? Operator)> iter = subExpressions;
@@ -104,12 +164,24 @@ namespace MathCalc
                 var minPrio = Operators.Min(x => x.Prio) - 1;
                 for (int i = maxPrio; i >= minPrio; i--)
                 {
-                    iter = Iter(iter, i).ToArray();
+                    iter = Iter(iter, i)
+#if DEBUG
+                        .ToArray() // Easier to debug when evaluated everting simultaneously 
+#endif
+                        ;
                 }
 
                 var (val, op) = iter.First();
                 return val;
             }
+
+            char[] endChars = evalMod switch
+            {
+                EvalMod.Root => [],
+                EvalMod.Brackets => [')'],
+                EvalMod.FunctionArgs => [',', ')'],
+                _ => [],
+            };
 
             for (; pos < expression.Length; pos++)
             {
@@ -120,69 +192,159 @@ namespace MathCalc
 
                 AssureLower(ref c);
 
-                if (!currentValue.HasValue)
+                switch (readingMod)
                 {
-                    if (c == '(')
-                    {
-                        pos++;
-                        currentValue = EvaluateExpression(ref pos, expression);
-                        pos--;
-                    }
-                    else if (c == ')')
-                    {
-                        if (buildingNumber != string.Empty)
-                            MakeBuildNumber();
-
-                        pos++;
-                        return CalcCurrentVal(pos - 1);
-                    }
-                    else if (char.IsAsciiDigit(c) || c == '.' || c == ',')
-                    {
-                        buildingNumber += c;
-                    }
-                    else if (buildingNumber != string.Empty)
-                    {
-                        MakeBuildNumber();
-                        pos--;
-                    }
-                    //else if (char.IsAsciiLetterLower(c))
-                    //{ 
-                    //    // Function like pow, sqrt, rnd or so
-                    //}
-                    else
-                    {
-                        throw new Exception("Invalid syntax: " + Sub(expression, startPos, pos));
-                    }
-                }
-                else
-                {
-                    if (c == ')')
-                    {
-                        if (buildingNumber != string.Empty)
-                            MakeBuildNumber();
-
-                        pos++;
-                        return CalcCurrentVal(pos - 1);
-                    }
-
-                    Operator? op = null;
-                    foreach (var x in Operators)
-                        if (SubStringEq(x.Expression, expression, ref pos))
+                    case ReadingMod.Brackets:
                         {
-                            op = x;
+                            if (c == ')')
+                            {
+                                readingMod = ReadingMod.Operator;
+                            }
+                            else
+                            {
+                                throw new Exception("Brackets not closed");
+                            }
+                            break;
+                        }
+                    case ReadingMod.Value:
+                        {
+                            if (c == '(')
+                            {
+                                pos++;
+                                currentValue = EvaluateExpression(ref pos, expression, EvalMod.Brackets);
+                                readingMod = ReadingMod.Brackets;
+
+                                //if (pos >= expression.Length)
+                                //    throw new Exception("Brackets not closed");
+
+                                pos -= 2;
+
+                            }
+                            else if (endChars.Contains(c)) // For (1 + 1) + 1
+                            {
+                                if (buildingValue != string.Empty)
+                                    MakeBuildNumber();
+
+                                pos++;
+                                return CalcCurrentVal(pos - 1);
+                            }
+                            else if (char.IsAsciiDigit(c) || c == '.' || c == ',')
+                            {
+                                buildingValue += c;
+                            }
+                            else if (buildingValue != string.Empty)
+                            {
+                                MakeBuildNumber();
+                                pos--;
+                            }
+                            else if (char.IsAsciiLetterLower(c))
+                            {
+                                buildingValue = string.Empty + c;
+                                readingMod = ReadingMod.FunctionName;
+                            }
+                            else
+                            {
+                                throw new Exception("Invalid syntax: " + Sub(expression, startPos, pos));
+                            }
+
                             break;
                         }
 
-                    if (op == null)
-                        throw new Exception("Invalid operator: " + Sub(expression, pos, pos + 1));
+                    case ReadingMod.Operator:
+                        {
+                            if (endChars.Contains(c)) // For ((1 + 1)) + 1
+                            {
+                                if (buildingValue != string.Empty)
+                                    MakeBuildNumber();
 
-                    subExpressions.Add((currentValue.Value, op));
+                                pos++;
+                                return CalcCurrentVal(pos - 1);
+                            }
 
-                    currentValue = null;
+                            Operator? op = null;
+                            foreach (var x in Operators)
+                                if (SubStringEq(x.Expression, expression, ref pos))
+                                {
+                                    op = x;
+                                    break;
+                                }
+
+                            if (op == null)
+                                throw new Exception("Invalid operator: " + Sub(expression, pos, pos + 1));
+
+                            subExpressions.Add((currentValue!.Value, op));
+
+                            currentValue = null;
+                            readingMod = ReadingMod.Value;
+                            break;
+                        }
+
+                    case ReadingMod.FunctionName:
+                        {
+                            if (char.IsAsciiLetterLower(c))
+                            {
+                                buildingValue += c;
+                            }
+                            else if (c == '(')
+                            {
+                                argValues.Clear();
+                                readingMod = ReadingMod.FunctionArgs;
+                            }
+                            else
+                            {
+                                throw new Exception("Invalid function: " + buildingValue + c);
+                            }
+                            break;
+                        }
+
+                    case ReadingMod.FunctionArgs:
+                        {
+                            if(c == ')')
+                                goto case ReadingMod.FunctionArgsComma;
+
+                            argValues.Add(EvaluateExpression(ref pos, expression, EvalMod.FunctionArgs));
+                            readingMod = ReadingMod.FunctionArgsComma;
+                            pos-=2;
+                            break;
+                        }
+                    case ReadingMod.FunctionArgsComma:
+                        {
+                            if (c == ',')
+                            {
+                                readingMod = ReadingMod.FunctionArgs;
+                            }
+                            else if (c == ')')
+                            {
+
+                                Function? fn = null;
+                                foreach (var x in Functions)
+                                    if (StringComparer.OrdinalIgnoreCase.Equals(x.Name, buildingValue) && x.CountAttributes == argValues.Count)
+                                    {
+                                        //pos += x.Name.Length;
+                                        fn = x;
+                                        break;
+                                    }
+
+                                if (fn == null)
+                                    throw new Exception($"Function not found with count arguments: {buildingValue} ({argValues.Count}x)");
+
+                                currentValue = fn?.Action.Invoke(argValues);
+                                buildingValue = string.Empty;
+                                readingMod = ReadingMod.Operator;
+                            }
+                            else
+                            {
+                                throw new Exception("Invalid function ending: " + c);
+                            }
+                            break;
+                        }
                 }
             }
 
-            if (buildingNumber != string.Empty)
+            if (evalMod != EvalMod.Root)
+                throw new Exception("Invalid syntax, symbol missing: " + string.Join(",", endChars));
+
+            if (buildingValue != string.Empty)
                 MakeBuildNumber();
 
             return CalcCurrentVal(pos);
